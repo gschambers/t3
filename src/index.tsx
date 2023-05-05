b6fbd757fd894714f31dcc64347170a4193db0b1
Gary Chambers <gary@repro.dev>
2023-05-05 18:37:48 +0100

    Attempt to override vercel live feedback widget

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
+function overrideVercelFeedbackWidget() {
+  const existingWidget = document.querySelector('vercel-live-feedback')
+
+  if (existingWidget) {
+    existingWidget.remove()
+  }
+
+  const widget = document.createElement('vercel-live-feedback')
+  widget.innerHTML = '<div>Surprise!</div>'
+  document.body.append(widget)
+}
+
 function main() {
   const domNode = document.getElementById('root')
 
@@ -11,6 +23,8 @@ function main() {
 
   const root = createRoot(domNode)
   root.render(<App />)
+
+  overrideVercelFeedbackWidget()
 }
 
 main()
